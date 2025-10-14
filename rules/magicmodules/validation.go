1f8f53e38e95450412e9577431bba27bc95478fe
arsiba <171977580+arsiba@users.noreply.github.com>
2025-10-14 15:17:42 +0000

    feat: change last terraform-linters import

--- rules/magicmodules/validation.go
+++ rules/magicmodules/validation.go
@@ -1,8 +1,8 @@
 package magicmodules
 
 import (
+	"github.com/arsiba/tofulint-ruleset-google/rules/magicmodules/verify"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-linters/tflint-ruleset-google/rules/magicmodules/verify"
 )
 
 const (
